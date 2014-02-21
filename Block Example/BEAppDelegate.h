388854db89162be5233585400145469e59e54c85
Anthony Picciano <anthony.picciano@gmail.com>
2014-02-21 15:52:54 -0700

    Initial Commit

---
+++ Block Example/BEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BEAppDelegate.h
+//  Block Example
+//
+//  Created by Anthony Picciano on 2/21/14.
+//  Copyright (c) 2014 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Block Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Block Example
+//
+//  Created by Anthony Picciano on 2/21/14.
+//  Copyright (c) 2014 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BEAppDelegate class]));
+    }
+}
